efce1c625ef350778d0f8bcb712f2fefc1f5209e
Agustin Gutierrez Mora <agugutierrezz27@gmail.com>
2025-04-10 05:20:22 +0000

    [Parte 2.2] Optimizacion de views y archivos html

--- repositorio/proyecto/polls/views.py
+++ repositorio/proyecto/polls/views.py
@@ -1,14 +1,29 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+    # Utilizando render no es necesario asignarle valor a una variable template, ni importar loader ni HttpResponse
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
+# Atajo 'get_object_or_404' para prácticas dónde haya excepción 404 si no hay objeto 
+# from django.shortcuts import get_object_or_404, render
+# def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
