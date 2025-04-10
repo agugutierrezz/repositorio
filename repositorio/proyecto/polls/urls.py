412bdbd673c20a8d87de456d625443fc824cd60a
Agustin Gutierrez Mora <agugutierrezz27@gmail.com>
2025-04-10 18:32:52 +0000

    [Parte 8] Correccion de error en polls/urls

--- repositorio/proyecto/polls/urls.py
+++ repositorio/proyecto/polls/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 from django.urls import include, path
-from django.conf import settings
-
-import debug_toolbar
 
 app_name = "polls"
 urlpatterns = [
@@ -12,8 +9,3 @@ urlpatterns = [
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
